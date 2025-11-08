c2ad73a04b46d8e0b23e35655d14bc59297ac592
mh152796 <mosarofdiu123@gmail.com>
2025-11-09 00:58:28 +0600

    Fix: Use compatible AGP and dependency versions

--- folioreader/src/main/java/com/folioreader/ui/activity/FolioActivity.kt
+++ folioreader/src/main/java/com/folioreader/ui/activity/FolioActivity.kt
@@ -255,6 +255,11 @@ class FolioActivity : AppCompatActivity(), FolioActivityCallback, MediaControlle
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+
         // Need to add when vector drawables support library is used.
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true)
 
